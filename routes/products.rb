ec5e0e2857eafc809269e7ea617858c84eaf9325
nikitasmall <nikitasosnov@yahoo.com>
2015-07-24 17:11:48 +0300

    added operations with count in cart

--- routes/products.rb
+++ routes/products.rb
@@ -20,19 +20,27 @@ module Sinatra
           end
 
           add_product_to_cart = lambda do
-            products = session[:cart] unless session[:cart].nil?
-            products ||= Hash.new(0) # try to initialize new cart
-            products[params[:id]] += 1 # count of line_items
+            session[:cart] ||= Hash.new(0) # try to initialize new cart
+            session[:cart][params[:id]] += 1 # count of line_items
 
-            session[:cart] = products
 
             redirect '/'
+          end
+
+          decrease_product_count_at_cart = lambda do
+            redirect '/cart' if session[:cart].nil?
+
+            session[:cart][params[:id]] -= 1
+            session[:cart].delete_if { |id, count| count <= 0 }
+
+            redirect '/cart'
           end
 
           app.get "/products/create", &create_form
           app.post "/products", &create_product
 
           app.post "/products/add/:id", &add_product_to_cart
+          app.post "/products/remove/:id", &decrease_product_count_at_cart
         end
 
       end
